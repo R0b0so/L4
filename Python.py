0d2a6adcfc3279beb95c3be21510144d709e127a
R0b0so <ethansagar222@gmail.com>
2024-10-02 19:38:58 -0700

    math

---
+++ Python.py
@@ -0,0 +1,6 @@
+math = int(input("enter your math mark:"))
+science = int(input("enter your science mark:"))
+english = int(input("enter your english mark:"))
+history = int(input("enter your history mark:"))
+average = (math + science + english + history)/4
+print(average)
